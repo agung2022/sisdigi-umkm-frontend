1010fc549083193a1f991e0c5bc0257410f969d1
Agung Faizal <105846443+agung2022@users.noreply.github.com>
2025-10-06 00:23:30 +0700

    Update RegisterPage.js

--- src/RegisterPage.js
+++ src/RegisterPage.js
@@ -24,7 +24,7 @@ export default function RegisterPage() {
         try {
             // sesuaikan field sesuai backend (misal name optional)
             const API_URL = process.env.REACT_APP_API_URL || "http://localhost:3001";
-      await axios.post(`${API_URL}api/auth/register`, { 
+      await axios.post('${API_URL}/api/auth/register', { 
                 name, 
                 email, 
                 password 
